721c36661bb3577178616b3f2b4fddf9d74e401d
Martin Mareš <maresmar@matfyz.cz>
2021-01-11 14:13:23 +0100

    CI: Fix crash on clients without `Accept-Language` header

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,8 +3,6 @@ from flask import url_for
 from flask.testing import FlaskClient
 from psan import app
 
-DEFAULT_HEADERS = [("Accept-Language", "en_US")]
-
 
 @pytest.fixture
 def client():
@@ -18,28 +16,28 @@ def test_index(client: FlaskClient) -> None:
     Test that index is accessible without login
     """
     with app.app_context():
-        response = client.get(url_for("index"), headers=DEFAULT_HEADERS)
+        response = client.get(url_for("index"))
     assert response.status_code == 200
     assert b"Revision:" in response.data
 
 
 def test_login(client: FlaskClient) -> None:
     with app.app_context():
-        response = client.get(url_for("auth.login"), headers=DEFAULT_HEADERS)
+        response = client.get(url_for("auth.login"))
     assert response.status_code == 200
     assert b"Log in" in response.data
 
 
 def test_register(client: FlaskClient) -> None:
     with app.app_context():
-        response = client.get(url_for("auth.register"), headers=DEFAULT_HEADERS)
+        response = client.get(url_for("auth.register"))
     assert response.status_code == 200
     assert b"Create a new account" in response.data
 
 
 def test_reset(client: FlaskClient) -> None:
     with app.app_context():
-        response = client.get(url_for("auth.reset"), headers=DEFAULT_HEADERS)
+        response = client.get(url_for("auth.reset"))
     assert response.status_code == 200
     assert b"Having trouble logging in?" in response.data
 
@@ -52,6 +50,6 @@ def test_restricted(client: FlaskClient) -> None:
 
 
 def login_redirect(page: str, client: FlaskClient) -> None:
-    response = client.get(page, headers=DEFAULT_HEADERS)
+    response = client.get(page)
     assert response.status_code == 302
     assert response.location == url_for("auth.login", _external=True)
