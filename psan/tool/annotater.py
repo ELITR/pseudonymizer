11438dff98a8857b1710d741592ce36d66b87add
Martin Mareš <maresmar@matfyz.cz>
2021-02-04 16:35:38 +0100

    rec: Memory optimalization for XML parsing

--- psan/tool/annotater.py
+++ psan/tool/annotater.py
@@ -54,6 +54,7 @@ class EvidenceParser(xml.sax.ContentHandler):
         # Word types and tokens
         self._lookup = word_lookup
         self._word_list: List[Word] = []
+        self._word_list_first_token_id = None
         self._lookup_events: List[LookupEvent] = []
         # Current state
         self._last_token_id = -1
@@ -79,26 +80,35 @@ class EvidenceParser(xml.sax.ContentHandler):
         if tag == "token":
             # Save last word info
             last_word = Word(self._last_token)
-            self._word_list.append(last_word)
-            # Lookup length (for following words)
+            # Future lookup length (for following words)
             type, lookup = self._lookup(last_word)
             if type != ReasonType.NONE:
                 target_id = self._last_token_id + lookup
                 self._registerLookup(self._last_token_id, target_id, type)
-            self._handleLookups(self._last_token_id)
+            # Current lookup event
+            if len(self._lookup_events) > 0:
+                if len(self._word_list) == 0:
+                    self._word_list_first_token_id = self._last_token_id
+                self._word_list.append(last_word)
+                self._handleLookups(self._last_token_id)
 
     def _registerLookup(self, current_id: int, target_id: int, type: ReasonType) -> None:
         heappush(self._lookup_events, LookupEvent(target_id, current_id, type))
 
     def _handleLookups(self, current_id: int):
-        if len(self._lookup_events) > 0 and self._lookup_events[0].target_token_id == current_id:
+        if self._lookup_events[0].target_token_id == current_id:
             event = heappop(self._lookup_events)
             # Prepare evidence
             candidate = Candidate(event.source_token_id, event.target_token_id)
             if event.type == ReasonType.WORD_TYPE:
-                types = [word.token for word in self._word_list[candidate.start:candidate.end + 1]]
+                types = [word.token for word in
+                         self._word_list[self._last_token_id + candidate.start:self._last_token_id + candidate.end + 1]]
                 evidence = Evidence(ReasonType.WORD_TYPE, candidate, types)
                 self._evidence_callback(evidence)
+            # Tokens cleanup
+            if len(self._lookup_events) == 0:
+                self._word_list_first_token_id = None
+                self._word_list = []
 
 
 class LookupEvent(NamedTuple):
