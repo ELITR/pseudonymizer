56ea71b1bb921c307a13734d8bc771d8da9bf5a7
Martin Mareš <maresmar@matfyz.cz>
2021-02-09 14:56:51 +0100

    admin: Rule removal UI

--- psan/rule.py
+++ psan/rule.py
@@ -3,7 +3,8 @@ import gettext
 from flask import Blueprint, jsonify, render_template, request
 
 from psan.auth import login_required
-from psan.db import get_cursor
+from psan.db import commit, get_cursor
+from psan.model import AnnotationDecision
 
 _ = gettext
 
@@ -37,3 +38,18 @@ def data():
                 row["condition"]), "decision": row["decision"]})
         # Return output
         return jsonify({"total": cursor.rowcount, "totalNotFiltered": not_filtered, "rows": rows})
+
+
+@bp.route("/remove/<int:rule_id>",  methods=['POST'])
+@login_required()
+def remove(rule_id: int):
+    with get_cursor() as cursor:
+        # Remove already made annotation
+        cursor.execute("UPDATE annotation SET rule = NULL, decision = %s WHERE rule = %s",
+                       (AnnotationDecision.UNDECIDED.value, rule_id))
+        # Remove rule
+        cursor.execute("DELETE FROM rule WHERE id = %s", (rule_id,))
+        commit()
+
+    # Return OK reply
+    return jsonify({"stutus": "ok"})
