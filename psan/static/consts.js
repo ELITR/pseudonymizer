1229d637fc1614647f68240e80f6a6388dd2b539
Martin Mareš <maresmar@matfyz.cz>
2021-02-23 10:55:11 +0100

    user: Handle NE type in JS

---
+++ psan/static/consts.js
@@ -0,0 +1,15 @@
+NE_CODES = {
+    "ah": "street numbers", "at": "phone/fax numbers", "az": "zip codes",
+    "gc": "states", "gh": "hydronyms", "gl": "nature areas / objects", "gq": "urban parts",
+    "gr": "territorial names", "gs": "streets, squares", "gt": "continents", "gu": "cities/towns",
+    "g_": "underspecified geographical name", "ia": "conferences/contests", "ic": "cult./educ./scient. inst.",
+    "if": "companies, concerns...", "io": "government/political inst.", "i_": "underspecified institutions",
+    "me": "email address", "mi": "internet links", "mn": "periodical", "ms": "radio and TV stations",
+    "na": "age", "nb": "vol./page/chap./sec./fig. numbers", "nc": "cardinal numbers",
+    "ni": "itemizer", "no": "ordinal numbers", "ns": "sport score", "n_": "underspecified number expression",
+    "oa": "cultural artifacts (books, movies)", "oe": "measure units", "om": "currency units",
+    "op": "products", "or": "directives, norms", "o_": "underspecified artifact name", "pc": "inhabitant names",
+    "pd": "(academic) titles", "pf": "first names", "pm": "second names", "pp": "relig./myth persons",
+    "ps": "surnames", "p_": "underspecified personal name", "td": "days", "tf": "feasts", "th": "hours",
+    "tm": "months", "ty": "years"
+}
