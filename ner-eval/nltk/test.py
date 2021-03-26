c43b9d6db9cb00518115e1062f0f10ec4d31de00
Martin Mareš <maresmar@matfyz.cz>
2021-03-26 18:49:57 +0100

    eval: Initial version

---
+++ ner-eval/nltk/test.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk.tag.stanford import StanfordNERTagger
+
+sentence = u"Twenty miles east of Reno, Nev., " \
+    "where packs of wild mustangs roam free through " \
+    "the parched landscape, Tesla Gigafactory 1 " \
+    "sprawls near Interstate 80."
+
+jar = './stanford-ner.jar'
+model = './english.all.3class.distsim.crf.ser.gz'
+
+# Prepare NER tagger with english model
+ner_tagger = StanfordNERTagger(model, jar, encoding='utf8')
+
+# Tokenize: Split sentence into words
+words = nltk.word_tokenize(sentence)
+
+# Run NER tagger on words
+print(ner_tagger.tag(words))
