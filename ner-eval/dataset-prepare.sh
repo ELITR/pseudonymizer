afe50e9a0d88adc4cc0a33fce923f11ca41aa1c0
Martin Mareš <maresmar@matfyz.cz>
2021-04-08 16:19:50 +0200

    eval: Data digger updates

--- ner-eval/dataset-prepare.sh
+++ ner-eval/dataset-prepare.sh
@@ -11,13 +11,26 @@ do
 	lang=$(jq -r .language <<< "${line}")
 	meeting=$(jq -r .meetingId <<< "${line}")
 	oid=$(jq -r '._id."$oid"' <<< "${line}")
+	date=$(jq -r '.createdAt."$date"' <<< "${line}")
 	name=$(jq -r .name <<< "${line}")
 
 	# Parsing
-	file=$lang/$meeting/${name}/$oid
+	file=$lang/$meeting/${name}/$date
 	mkdir -p "$(dirname "$file")"
 	echo -n "Writing $file"
 	jq -r '.annotated_content' <<< "${line}" > "$file"
 	#jq -r '.content' <<< "${line}" > "$file"
 	echo " ($(cat "$file" | wc -w) words)"
+done
+
+# Data info
+echo "Found $(find -mindepth 3 -type d | wc -l) texts"
+
+# Clear duplicities (leaving only newest files)
+find -mindepth 3 -type d | while read file; do
+    todo=$(ls "$file" | head -n-1 | wc -l)
+	if [ $todo -gt 0 ]; then
+		echo "Removing $todo duplicties from $file"
+		ls "$file" | head -n-1 | awk "{print \"$file/\"\$1}" | xargs -d "\n" rm
+	fi
 done
