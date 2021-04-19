8f36b86abfa990ac73162c1e63554cf572a4c7f4
Martin Mareš <maresmar@matfyz.cz>
2021-04-19 20:06:56 +0200

    eval: Improved summary generator

--- ner-eval/ner-eval.sh
+++ ner-eval/ner-eval.sh
@@ -13,3 +13,9 @@ for ner in "nametag" "nltk" "spacy"; do
 	echo $ner
 	find -name input.txt -path "./dataset/*" | outputPipe $ner | python3 ner_digger.py $ner 
 done
+
+mkdir -p ./dataset/out/
+find "./dataset" -name input.txt -exec dirname {} \; | while read -r folder; do
+	output="./dataset/out/$(echo "$folder" | cut -d"/" -f 4,5 | tr "/" "-").csv"
+	find "$folder" -name "*.csv" | grep -v features.csv | xargs -d "\n" python3 summary_builder.py "$output" "$folder/features.csv"
+done
