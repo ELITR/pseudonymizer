61f90e86aac46c236e849afd11dc34962db5bf27
Martin Mareš <maresmar@matfyz.cz>
2021-04-22 14:43:25 +0200

    eval: Summary improvements

--- ner-eval/ner-eval.sh
+++ ner-eval/ner-eval.sh
@@ -15,11 +15,12 @@ for ner in "nametag" "nltk" "spacy"; do
 done
 
 # Summary
-echo "Summary report"
+echo "Building summary..."
 mkdir -p ./dataset/out/
-echo "features, nametag-exact, nametag-inside, nametag-partial, nametag-lines, nltk-exact, nltk-inside, nltk-partial, nltk-lines, spacy-exact, spacy-inside, spacy-partial, spacy-lines" > "./dataset/out/summary.csv" 
+echo "features,nametag-exact,nametag-inside,nametag-partial,nametag-lines,nltk-exact,nltk-inside,nltk-partial,nltk-lines,spacy-exact,spacy-inside,spacy-partial,spacy-lines" > "./dataset/out/summary.csv" 
 find "./dataset" -name input.txt -exec dirname {} \; | while read -r folder; do
 	test_name=$(echo "$folder" | cut -d"/" -f 4,5 | tr "/" "-")
 	output="./dataset/out/$test_name.csv"
 	find "$folder" -name "*.csv" | grep -v features.csv | xargs -d "\n" python3 summary_builder.py "$output" "$folder/features.csv" >> "./dataset/out/summary.csv"
 done
+echo "... $(wc -l ./dataset/out/summary.csv) found"
